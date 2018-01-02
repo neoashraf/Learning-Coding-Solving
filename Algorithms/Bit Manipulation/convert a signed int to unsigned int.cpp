aab4703f84c836ed1d7919382fd823e83fef9329
neoashraf <ashrafuddin160@gmail.com>
2018-01-02 22:35:17 +0600

    Bit manipulation -  one problem added

---
+++ Algorithms/Bit Manipulation/convert a signed int to unsigned int.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double dd;
+
+#define     mem(x,y)   memset(x,y,sizeof(x))
+#define     PB(x)      push_back(x)
+#define     POB()      pop_back()
+#define     SZ(a)      a.size()
+#define     len(a)     strlen(a)
+#define     SQR(a)     (a*a)
+#define     dig(a)     a-'0'
+#define     all(v)     v.begin(),v.end()
+#define     read(a)    scanf("%d",&a)
+#define     read2(a,b) scanf("%d %d",&a,&b)
+#define     read3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define     cread(a)    scanf("%I64d",&a)
+#define     cread2(a,b) scanf("%I64d %I64d",&a,&b)
+#define     cread3(a,b,c) scanf("%I64d %I64d %I64d",&a,&b,&c)
+#define     write(a)   printf("%d",a);
+#define     nl         printf("\n")
+#define     fr(i,a,b)  for(i=a;i<=b;i++)
+#define     rfr(i,a,b) for(i=a;i>=b;i--)
+#define     nfr(i,a,b)  for(i=a;i<b;i++)
+#define     nrfr(i,a,b) for(i=a;i>b;i--)
+#define     sf  scanf
+#define     pf  printf
+#define     mkp make_pair
+#define     fs  first
+#define     sd  second
+
+#define     inf  1<<25
+#define     sz   1000006
+#define     primeLimit  1000005
+#define     eps  1e-9
+#define     mod  1000000007
+#define     PI   2.0*acos(0.0)
+#define     imax 2147483647
+#define     lmax 9223372036854775807LL
+
+/// signed limit ( -2^31 to 2^31 -1)  and unsigned limit ( 0 to 2^32 -1)
+
+/// this shows what a signed integer becomes in unsigned integer
+/// a signed number -x equals an unsigned number (2^n - x)   ( we take negatives from (-2^31 to -1) and see what it becomes )
+
+/// main theory behind it is : A number + The numbers two's complement = 2^n (here n =32)
+/// so if input is x output is its two's complement. ( in decimal )
+
+int main() {
+    int x = -43;
+    unsigned int y = x;
+    cout << x << "\n"; // -43
+    cout << y << "\n"; // 4294967253  2^n = 4294967296
+    return 0;
+}
