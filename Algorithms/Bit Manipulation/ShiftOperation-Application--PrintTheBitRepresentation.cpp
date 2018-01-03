892ac9b3602f18c81f538a3e1af2905409a7f7db
neoashraf <ashrafuddin160@gmail.com>
2018-01-04 02:14:00 +0600

    Shift application added

---
+++ Algorithms/Bit Manipulation/ShiftOperation-Application--PrintTheBitRepresentation.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double dd;
+
+#define     mem(x,y)   memset(x,y,sizeof(x))
+#define     PB(x)      push_back(x)
+#define     POB()      pop_back()
+#define     SZ(a)      a.size()
+#define     len(a)     strlen(a)
+#define     SQR(a)     (a*a)
+#define     dig(a)     a-'0'
+#define     all(v)     v.begin(),v.end()
+#define     read(a)    scanf("%d",&a)
+#define     read2(a,b) scanf("%d %d",&a,&b)
+#define     read3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define     cread(a)    scanf("%I64d",&a)
+#define     cread2(a,b) scanf("%I64d %I64d",&a,&b)
+#define     cread3(a,b,c) scanf("%I64d %I64d %I64d",&a,&b,&c)
+#define     write(a)   printf("%d",a);
+#define     nl         printf("\n")
+#define     fr(i,a,b)  for(i=a;i<=b;i++)
+#define     rfr(i,a,b) for(i=a;i>=b;i--)
+#define     nfr(i,a,b)  for(i=a;i<b;i++)
+#define     nrfr(i,a,b) for(i=a;i>b;i--)
+#define     sf  scanf
+#define     pf  printf
+#define     mkp make_pair
+#define     fs  first
+#define     sd  second
+
+#define     inf  1<<25
+#define     sz   1000006
+#define     primeLimit  1000005
+#define     eps  1e-9
+#define     mod  1000000007
+#define     PI   2.0*acos(0.0)
+#define     imax 2147483647
+#define     lmax 9223372036854775807LL
+
+/// The following code prints the bit representation of an integer number x:
+/// Here we print the 32-bit representation
+
+/// Explanation :
+/// To get bit representation we iterate from left to right (31 to 0).
+/// At every position i we perform an add operation between x and (1<<i).
+/// 1<<i has its only 1 bit in ith position.
+/// So if the number has an 1 in ith position & operation yields 1 else 0.
+/// After the loop completes we get the whole representation.
+
+int main() {
+    int x,i;
+    read(x);
+    cout<<x<<" in binary : ";
+    rfr(i,31,0){
+        if(x&(1<<i))
+            cout<<"1";
+        else
+            cout<<"0";
+    }
+    cout<<endl;
+    return 0;
+}
