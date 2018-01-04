812f4fe15f719f4a77c70ffb199c3ddd2f963dca
neoashraf <ashrafuddin160@gmail.com>
2018-01-04 19:38:41 +0600

    one code added

---
+++ Algorithms/Bit Manipulation/BuilinFunctions.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double dd;
+
+#define     mem(x,y)   memset(x,y,sizeof(x))
+#define     PB(x)      push_back(x)
+#define     POB()      pop_back()
+#define     SZ(a)      a.size()
+#define     len(a)     strlen(a)
+#define     SQR(a)     (a*a)
+#define     dig(a)     a-'0'
+#define     all(v)     v.begin(),v.end()
+#define     read(a)    scanf("%d",&a)
+#define     read2(a,b) scanf("%d %d",&a,&b)
+#define     read3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define     cread(a)    scanf("%I64d",&a)
+#define     cread2(a,b) scanf("%I64d %I64d",&a,&b)
+#define     cread3(a,b,c) scanf("%I64d %I64d %I64d",&a,&b,&c)
+#define     write(a)   printf("%d",a);
+#define     nl         printf("\n")
+#define     fr(i,a,b)  for(i=a;i<=b;i++)
+#define     rfr(i,a,b) for(i=a;i>=b;i--)
+#define     nfr(i,a,b)  for(i=a;i<b;i++)
+#define     nrfr(i,a,b) for(i=a;i>b;i--)
+#define     sf  scanf
+#define     pf  printf
+#define     mkp make_pair
+#define     fs  first
+#define     sd  second
+
+#define     inf  1<<25
+#define     sz   1000006
+#define     primeLimit  1000005
+#define     eps  1e-9
+#define     mod  1000000007
+#define     PI   2.0*acos(0.0)
+#define     imax 2147483647
+#define     lmax 9223372036854775807LL
+
+/// __builtin_clz(x): the number of zeros at the beginning of the number
+/// __builtin_ctz(x): the number of zeros at the end of the number
+/// __builtin_popcount(x): the number of ones in the number
+/// __builtin_parity(x): the parity (even or odd) of the number of ones   /// if odd 1 if even 0
+
+int main() {
+    int x = 1;
+    cout<<__builtin_clz(x)<<endl;
+    cout<<__builtin_ctz(x)<<endl;
+    cout<<__builtin_popcount(x)<<endl;
+    cout<< __builtin_parity(x)<<endl;
+    return 0;
+}
