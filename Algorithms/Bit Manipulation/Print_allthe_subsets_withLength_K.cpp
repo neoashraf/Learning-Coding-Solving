2490ccdafac3fd68c059bc6a194e590f43c0aee2
neoashraf <ashrafuddin160@gmail.com>
2018-01-06 03:41:34 +0600

    print subset of fixed length

---
+++ Algorithms/Bit Manipulation/Print_allthe_subsets_withLength_K.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double dd;
+
+#define     mem(x,y)   memset(x,y,sizeof(x))
+#define     PB(x)      push_back(x)
+#define     POB()      pop_back()
+#define     SZ(a)      a.size()
+#define     len(a)     strlen(a)
+#define     SQR(a)     (a*a)
+#define     dig(a)     a-'0'
+#define     all(v)     v.begin(),v.end()
+#define     read(a)    scanf("%d",&a)
+#define     read2(a,b) scanf("%d %d",&a,&b)
+#define     read3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define     cread(a)    scanf("%I64d",&a)
+#define     cread2(a,b) scanf("%I64d %I64d",&a,&b)
+#define     cread3(a,b,c) scanf("%I64d %I64d %I64d",&a,&b,&c)
+#define     write(a)   printf("%d",a);
+#define     nl         printf("\n")
+#define     fr(i,a,b)  for(i=a;i<=b;i++)
+#define     rfr(i,a,b) for(i=a;i>=b;i--)
+#define     nfr(i,a,b)  for(i=a;i<b;i++)
+#define     nrfr(i,a,b) for(i=a;i>b;i--)
+#define     sf  scanf
+#define     pf  printf
+#define     mkp make_pair
+#define     fs  first
+#define     sd  second
+
+#define     inf  1<<25
+#define     sz   1000006
+#define     primeLimit  1000005
+#define     eps  1e-9
+#define     mod  1000000007
+#define     PI   2.0*acos(0.0)
+#define     imax 2147483647
+#define     lmax 9223372036854775807LL
+
+/// The following code goes through the subsets with exactly k elements
+
+int main() {
+    int n,k;
+    read2(n,k);
+    for(int b = 0; b <(1<<n); b++){         /// total subset possible 2^n - 1
+        /// process subset of b
+        if(__builtin_popcount(b) == k){   /// subset length check
+            for(int i=0;i<32;i++){
+                /// for every binary representation of b if a bit position is 1 the position is printed. This position are the elements of the subset
+                if(b & (1<<i))
+                    cout<<i<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
