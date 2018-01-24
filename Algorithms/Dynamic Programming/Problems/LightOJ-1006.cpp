d2b474d2990a3221e0ab46f758a2d0b6cf81a559
neoashraf <ashrafuddin160@gmail.com>
2018-01-24 12:26:32 +0600

    Recursuon problem optimization using DP lightoj-1006

---
+++ Algorithms/Dynamic Programming/Problems/LightOJ-1006.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double dd;
+
+#define     mem(x,y)   memset(x,y,sizeof(x))
+#define     PB(x)      push_back(x)
+#define     POB()      pop_back()
+#define     SZ(a)      a.size()
+#define     len(a)     strlen(a)
+#define     SQR(a)     (a*a)
+#define     dig(a)     a-'0'
+#define     all(v)     v.begin(),v.end()
+#define     read(a)    scanf("%d",&a)
+#define     read2(a,b) scanf("%d %d",&a,&b)
+#define     read3(a,b,c) scanf("%d %d %d",&a,&b,&c)
+#define     cread(a)    scanf("%I64d",&a)
+#define     cread2(a,b) scanf("%I64d %I64d",&a,&b)
+#define     cread3(a,b,c) scanf("%I64d %I64d %I64d",&a,&b,&c)
+#define     write(a)   printf("%d",a);
+#define     nl         printf("\n")
+#define     fr(i,a,b)  for(i=a;i<=b;i++)
+#define     rfr(i,a,b) for(i=a;i>=b;i--)
+#define     nfr(i,a,b)  for(i=a;i<b;i++)
+#define     nrfr(i,a,b) for(i=a;i>b;i--)
+#define     sf  scanf
+#define     pf  printf
+#define     mkp make_pair
+#define     fs  first
+#define     sd  second
+
+#define     inf  1<<25
+#define     sz   1000006
+#define     primeLimit  1000005
+#define     eps  1e-9
+#define     mod  10000007
+#define     PI   2.0*acos(0.0)
+#define     imax 2147483647
+#define     lmax 9223372036854775807LL
+
+int a, b, c, d, e, f,dp[10006];
+int fn( int n ) {
+    if( n == 0 ) return a;
+    if( n == 1 ) return b;
+    if( n == 2 ) return c;
+    if( n == 3 ) return d;
+    if( n == 4 ) return e;
+    if( n == 5 ) return f;
+    if( dp[n]!= -1) return dp[n];
+    return dp[n] = ( fn(n-1)%mod + fn(n-2)%mod + fn(n-3)%mod + fn(n-4)%mod + fn(n-5)%mod + fn(n-6)%mod )%mod;
+}
+int main() {
+    int n, caseno = 0, cases;
+    scanf("%d", &cases);
+    while( cases-- ) {
+        scanf("%d %d %d %d %d %d %d", &a, &b, &c, &d, &e, &f, &n);
+        mem(dp,-1);
+        printf("Case %d: %d\n", ++caseno, fn(n) % mod);
+    }
+    return 0;
+}
